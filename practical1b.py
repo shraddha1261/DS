a8c8a8dc40a65656d5e4626ae0befcd6aed1132e
shraddha1261 <69567935+shraddha1261@users.noreply.github.com>
2020-09-15 18:24:33 +0530

    Create practical1b.py

---
+++ practical1b.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+x = np.array([[12,3,21],[2,43,5],[5,32,53]])
+y = np.array([[11,4,6],[21,7,9],[4,8,43]])
+a = np.multiply(x,y)
+b = np.add(x,y)
+c = np.transpose(x)
+d = np.transpose(y)
+print("Multiplication of two matrices")
+print(a)
+print("Addition of two matrices")
+print(b)
+print("Transpose of two matrices")
+print(c)
+print(d)
